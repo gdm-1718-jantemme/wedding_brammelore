b904607e3eb072da82be50400c0e708022a88b99
Jan Temmerman <jantemme@student.arteveldehs.be>
2021-01-26 18:31:52 +0100

    made formModal a seperate component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,31 +6,15 @@ import { Checkmark } from 'react-checkmark';
 
 import './App.sass';
 import CodeModal from './components/modals/codeModal';
+import FormModal from './components/modals/formModal';
 
 function App() {
-  const [code, setCode] = useState('');
-  const [firstname, setFirstname] = useState('');
-  const [lastname, setLastname] = useState('');
-  const [email, setEmail] = useState('');
-  const [personAmount, setPersonAmount] = useState(0);
-  const [isLoading, setIsLoading] = useState(false);
   const [receptionIsGoing, setReceptionIsGoing] = useState(false);
   const [dinerIsGoing, setDinerIsGoing] = useState(false);
   const [partyIsGoing, setPartyIsGoing] = useState(false);
   //const [emailField, setEmailField] = useState(<></>);
   const [festivitiesCheckboxes, setFestivitiesCheckboxes] = useState(<></>);
-  const [firstNameWarning, setFirstNameWarning] = useState(<></>);
-  const [lastnameWarning, setLastnameWarning] = useState(<></>);
-  const [festivitiesWarning, setFestivitiesWarning] = useState(<></>);
-  const [amountWarning, setAmountWarning] = useState(<></>);
-  const [emailWarning, setEmailWarning] = useState(<></>);
   const [checkmark, setCheckmark] = useState(<></>);
-
-  useEffect(() => {
-    if (personAmount > 5) setPersonAmount(5);
-    else if (personAmount < 0) setPersonAmount(0);
-    return;
-  }, [personAmount]);
 
   const renderFestivities = (festivities) => {
     const toRender = festivities.map((festivity) => {
@@ -65,8 +49,7 @@ function App() {
     }
   };
 
-  const animate = () => {
-    console.log('animating');
+  const showFormModal = () => {
     document.getElementById('left_half').classList.add('fullWidth');
     document.getElementById('code-modal').classList.add('disappear');
     setTimeout(() => {
@@ -74,233 +57,29 @@ function App() {
     }, 750);
   };
 
-  const handleEnter = (e) => {
-    if (e.keyCode === 13) {
-      const form = e.target.form;
-      const index = Array.prototype.indexOf.call(form, e.target);
-      form.elements[index + 1].focus();
-      e.preventDefault();
-    }
+  const showFinishModal = () => {
+    setTimeout(() => {
+      document.getElementById('form-modal').classList.remove('appear');
+      setTimeout(() => {
+        document.getElementById('form-modal').style.display = 'none';
+      }, 500); // Setting display to none because overflow issue on iphone 5
+      document.getElementById('finish-modal').classList.add('appear');
+      //document.getElementById('left_half').classList.remove('fullWidth');
+      setCheckmark(<Checkmark size="60px" color="#223344" />);
+    }, 1500); // Most connections are so fast the loading spinner wont even trigger, so i add a delay to let the user know something's happening/happened already
   };
-
-  const isImpossible = () => {
-    if (personAmount > 0 && !receptionIsGoing && !dinerIsGoing && !partyIsGoing) return true;
-    else if (personAmount === 0 && (receptionIsGoing || dinerIsGoing || partyIsGoing)) return true;
-    else return false;
-  };
-
-  const isEmailValid = (emailString) => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(emailString).toLowerCase());
-  };
-
-  const checkForm = () => {
-    if (firstname === '' || lastname === '' || !isEmailValid(email) || isImpossible()) return false;
-    else return true;
-  };
-
-  const clearAllWarnings = () => {
-    document.getElementById('firstname-input').classList.remove('wrong');
-    document.getElementById('lastname-input').classList.remove('wrong');
-    document.getElementById('email-input').classList.remove('wrong');
-    document.getElementById('amount-input').classList.remove('wrong');
-    document.getElementById('checkboxes-container').classList.remove('wrong');
-    setFirstNameWarning(<></>);
-    setLastnameWarning(<></>);
-    setFestivitiesWarning(<></>);
-    setAmountWarning(<></>);
-    setEmailWarning(<></>);
-  };
-
-  const submitForm = () => {
-    if (checkForm()) {
-      clearAllWarnings();
-      setIsLoading(true);
-
-      fetch('http://localhost:5000/api/attendees/add', {
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        method: 'POST',
-        body: JSON.stringify({
-          name: firstname,
-          surname: lastname,
-          reception: {
-            isGoing: receptionIsGoing,
-            attendees: personAmount,
-          },
-          diner: {
-            isGoing: dinerIsGoing,
-            attendees: personAmount,
-          },
-          party: {
-            isGoing: partyIsGoing,
-            attendees: personAmount,
-          },
-          email: email,
-        }),
-      })
-        .then((res) => res.json())
-        .then((res) => {
-          console.log(res);
-          setTimeout(() => {
-            setIsLoading(false);
-            document.getElementById('form-modal').classList.remove('appear');
-            setTimeout(() => {
-              document.getElementById('form-modal').style.display = 'none';
-            }, 500); // Setting display to none because overflow issue on iphone 5
-            document.getElementById('finish-modal').classList.add('appear');
-            //document.getElementById('left_half').classList.remove('fullWidth');
-            setCheckmark(<Checkmark size="60px" color="#223344" />);
-          }, 1500); // Most connections are so fast the loading spinner wont even trigger, so i add a delay to let the user know something's happening/happened already
-
-          if (res.data.accepted) {
-            renderFestivities(res.data.festivities);
-            animate();
-            inputContainer.classList.remove('wrong');
-          } else {
-            inputContainer.classList.add('wrong');
-            inputContainer.classList.add('wrong-animation');
-            setTimeout(() => {
-              inputContainer.classList.remove('wrong-animation');
-            }, 820);
-          }
-        })
-        .catch((err) => console.log(err));
-    } else {
-      console.log('Form not correct.');
-      if (firstname === '') {
-        document.getElementById('firstname-input').classList.add('wrong');
-        setFirstNameWarning(<em className="wrong-text">Verplicht</em>);
-      } else {
-        document.getElementById('firstname-input').classList.remove('wrong');
-        setFirstNameWarning(<></>);
-      }
-      if (lastname === '') {
-        document.getElementById('lastname-input').classList.add('wrong');
-        setLastnameWarning(<em className="wrong-text">Verplicht</em>);
-      } else {
-        document.getElementById('lastname-input').classList.remove('wrong');
-        setLastnameWarning(<></>);
-      }
-      if (!isEmailValid(email)) {
-        document.getElementById('email-input').classList.add('wrong');
-        if (email === '') setEmailWarning(<em className="wrong-text">Verplicht</em>);
-        else setEmailWarning(<em className="wrong-text">Geen geldig emailadres</em>);
-      } else {
-        document.getElementById('email-input').classList.remove('wrong');
-        setEmailWarning(<></>);
-      }
-      if (isImpossible()) {
-        document.getElementById('amount-input').classList.add('wrong');
-        document.getElementById('checkboxes-container').classList.add('wrong');
-        if (personAmount > 0 && !receptionIsGoing && !dinerIsGoing && !partyIsGoing) {
-          document.getElementById('checkboxes-container').classList.add('wrong');
-          setFestivitiesWarning(<em className="wrong-text">Geen aanwezigheid aangeduid</em>);
-          setAmountWarning(<></>);
-          document.getElementById('amount-input').classList.remove('wrong');
-        } else if (personAmount === 0 && (receptionIsGoing || dinerIsGoing || partyIsGoing)) {
-          document.getElementById('amount-input').classList.add('wrong');
-          setAmountWarning(<em className="wrong-text">Geef in met hoeveel personen je komt</em>);
-          setFestivitiesWarning(<></>);
-          document.getElementById('checkboxes-container').classList.remove('wrong');
-        }
-      } else {
-        document.getElementById('amount-input').classList.remove('wrong');
-        document.getElementById('checkboxes-container').classList.remove('wrong');
-        setFestivitiesWarning(<></>);
-        setAmountWarning(<></>);
-      }
-    }
-  };
-
-  /*
-  const toggleEmailField = () => {
-    const checkbox = document.getElementById('confirmation');
-
-    if (checkbox.checked)
-      setEmailField(
-        <>
-          <p>Email</p>
-          <div className="input-container">
-            <input type="email" placeholder="Bv. hannelore@gmail.com" />
-          </div>
-        </>,
-      );
-    else setEmailField(<></>);
-  };
-  */
 
   return (
     <div className="App">
-      <CodeModal nextModal={animate} renderFestivities={renderFestivities} />
+      <CodeModal nextModal={showFormModal} renderFestivities={renderFestivities} />
 
-      <div id="form-modal" className="form-modal">
-        <form className="form-container" autoComplete="on">
-          <p>Voornaam {firstNameWarning}</p>
-          <div className="input-container" id="firstname-input">
-            <input
-              type="text"
-              placeholder="Bv. Hannelore"
-              value={firstname}
-              onChange={(e) => setFirstname(e.target.value)}
-              onKeyDown={(e) => handleEnter(e)}
-              autoComplete="given-name"
-            />
-          </div>
-          <p>Achternaam {lastnameWarning}</p>
-          <div className="input-container" id="lastname-input">
-            <input
-              type="text"
-              placeholder="Bv. Temmerman"
-              value={lastname}
-              onChange={(e) => setLastname(e.target.value)}
-              autoComplete="family-name"
-            />
-          </div>
-          <p>Aanwezig bij {festivitiesWarning}</p>
-          <div className="checkboxes-container" id="checkboxes-container">
-            {festivitiesCheckboxes}
-          </div>
-          <p>Aantal personen {amountWarning}</p>
-          <div className="input-container" id="amount-input">
-            <input
-              type="number"
-              min={0}
-              max={5}
-              value={personAmount}
-              onChange={(e) => setPersonAmount(parseInt(e.target.value))}
-            />
-          </div>
-          <p>Email {emailWarning}</p>
-          <div className="input-container" id="email-input">
-            <input
-              type="email"
-              placeholder="Bv. hannelore@gmail.com"
-              autoComplete="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          {/*
-          <div className="checkbox_mail-container">
-            <input
-              type="checkbox"
-              id="confirmation"
-              name="confirmation"
-              value="confirmation"
-              onChange={() => toggleEmailField()}
-            ></input>
-            <label htmlFor="confirmation">Ik wil een bevestigingsmail ontvangen</label>
-          </div>
-          */}
-        </form>
-        <div className="divider" />
-        <button id="submit" className="submit-button" onClick={() => submitForm()}>
-          {isLoading ? <PulseLoader color={'#7C5B34'} loading={true} size={6} /> : 'Versturen'}
-        </button>
-      </div>
+      <FormModal
+        showFinishModal={showFinishModal}
+        festivitiesCheckboxes={festivitiesCheckboxes}
+        partyIsGoing={partyIsGoing}
+        dinerIsGoing={dinerIsGoing}
+        receptionIsGoing={receptionIsGoing}
+      />
 
       <div id="finish-modal" className="finish-modal">
         <div>
